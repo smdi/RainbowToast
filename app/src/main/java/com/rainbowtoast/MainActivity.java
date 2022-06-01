3086ffc684287718e656191fbbf602b573403ba8
Imran <smdimran838@gmail.com>
2022-06-01 20:14:02 +0530

    Added data of lite Rainbow Toast, forward for rainbow design if it doesn't effect the performance

--- app/src/main/java/com/rainbowtoast/MainActivity.java
+++ app/src/main/java/com/rainbowtoast/MainActivity.java
@@ -1,42 +1,19 @@
 package com.rainbowtoast;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //add enum for type and interface for duration
-        showToast(MainActivity.this, "Success", "Connection Established Successfully!", 4, "Success" );
-    }
 
-    public void showToast(Activity activity, String titleData, String messageData, int duration, String type) {
-
-        View view = LayoutInflater.from(activity)
-                    .inflate(R.layout.rainbow_toast_layout, null);
-
-        TextView title = view.findViewById(R.id.rainbowTitle);
-        title.setText(titleData);
-
-        TextView message = view.findViewById(R.id.rainbowMessage);
-        message.setText(messageData);
-
-        //check type and add colors
-        Toast toast = new Toast(activity);
-        toast.setDuration(duration);
-        toast.setGravity(Gravity.BOTTOM, 0, 0);
-        toast.setView(view);
-        toast.show();
+        RainbowToast.showToast(this, "Delete", "Connection terminated successfully!",
+                RainbowToast.LENGTH_LONG, RainbowToast.CUSTOM, RainbowToast.LITE );
     }
 }
---
+++ app/src/main/java/com/rainbowtoast/RainbowToast.java
@@ -0,0 +1,108 @@
+package com.rainbowtoast;
+
+import android.app.Activity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.card.MaterialCardView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class RainbowToast {
+
+    private static int colorChooser = 0;
+    private static final int DELAY_TIMER = 100;
+    private static int cancelTimer = DELAY_TIMER;
+    private static final int RUN_INTERVAL = 200;
+    private static final int STOP_AFTER = 4000;
+    public static final String SUCCESS = "SUCCESS";
+    public static final String ERROR = "ERROR";
+    public static final String WARNING = "WARNING";
+    public static final String INFO = "INFO";
+    public static final String CUSTOM = "CUSTOM";
+    public static final String LITE = "LITE";
+    public static final String DARK = "DARK";
+    public static final int LENGTH_SHORT = 2;
+    public static final int LENGTH_LONG = 4;
+
+    public static void showToast(Activity activity, String titleData, String messageData, int duration, String type, String mode) {
+
+        View view = LayoutInflater.from(activity)
+                .inflate(R.layout.rainbow_toast_layout, null);
+        Timer timer = new Timer();
+        MaterialCardView materialCardView = view.findViewById(R.id.rainbowCard);
+        TextView title = view.findViewById(R.id.rainbowTitle);
+        TextView message = view.findViewById(R.id.rainbowMessage);
+        Toast toast = new Toast(activity);
+
+
+        setColorByType(view, activity, type, mode, materialCardView);
+//        callColorChanger(view, activity, materialCardView, timer);
+        title.setText(titleData);
+        message.setText(messageData);
+        toast.setDuration(duration);
+        toast.setGravity(Gravity.BOTTOM, 0, 0);
+        toast.setView(view);
+        toast.show();
+    }
+
+    private static void setColorToCard(MaterialCardView materialCardView, Activity activity,int colorBackground, int colorStroke){
+        materialCardView.setCardBackgroundColor(activity.getResources().getColor(colorBackground));
+        materialCardView.setStrokeColor(activity.getResources().getColor(colorStroke));
+    }
+
+    private static void setColorByType(View view, Activity activity, String type, String mode, MaterialCardView materialCardView) {
+
+
+        if(mode.equals(DARK)){
+
+        }else{
+            switch (type) {
+                case SUCCESS:
+                    setColorToCard(materialCardView, activity, R.color.card_backgroud_success_lite, R.color.card_stroke_success_lite);
+                    break;
+                case ERROR:
+                    setColorToCard(materialCardView, activity, R.color.card_backgroud_error_lite, R.color.card_stroke_error_lite);
+                    break;
+                case WARNING:
+                    setColorToCard(materialCardView, activity, R.color.card_backgroud_warning_lite, R.color.card_stroke_warning_lite);
+                    break;
+                case INFO:
+                    setColorToCard(materialCardView, activity, R.color.card_backgroud_info_lite, R.color.card_stroke_info_lite);
+                    break;
+                default:
+                    setColorToCard(materialCardView, activity, R.color.card_backgroud_custom_lite, R.color.card_stroke_custom_lite);
+                    break;
+            }
+        }
+
+    }
+
+    private static void callColorChanger(View view, Activity activity, MaterialCardView materialCardView, Timer timer){
+
+        timer.scheduleAtFixedRate(new TimerTask(){
+            @Override
+            public void run(){
+                if(colorChooser == 0){
+
+                    colorChooser = 1;
+                }
+                else if(colorChooser == 1){
+                    colorChooser = 0;
+
+                }
+                cancelTimer += RUN_INTERVAL;
+                if(cancelTimer >= STOP_AFTER){
+                    timer.cancel();
+                }
+            }
+        },DELAY_TIMER,RUN_INTERVAL);
+
+    }
+
+}
