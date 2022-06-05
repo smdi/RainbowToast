28bfcb126313a183e8a5a43c575b1ae3a78fda67
Imran <smdimran838@gmail.com>
2022-06-05 12:58:39 +0530

    Re-added the color transition and forward for dark design

--- app/src/main/java/com/rainbowtoast/MainActivity.java
+++ app/src/main/java/com/rainbowtoast/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        RainbowToast.showToast(this, "Delete", "Connection terminated successfully!",
+        RainbowToast.showToast(this, "Treat", "You have won chocolates and candy!",
                 RainbowToast.LENGTH_LONG, RainbowToast.LITE );
     }
 }
--- app/src/main/java/com/rainbowtoast/RainbowToast.java
+++ app/src/main/java/com/rainbowtoast/RainbowToast.java
@@ -9,10 +9,17 @@ import android.widget.Toast;
 
 import com.google.android.material.card.MaterialCardView;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class RainbowToast {
 
-
+    private static int colorChooser = 0;
+    private static final int DELAY_TIMER = 0;
+    private static int cancelTimer = DELAY_TIMER;
+    private static final int RUN_INTERVAL = 1000;
+    private static final int STOP_AFTER = 4000;
     public static final String LITE = "LITE";
     public static final String DARK = "DARK";
     public static final int LENGTH_SHORT = 2;
@@ -22,12 +29,17 @@ public class RainbowToast {
 
         View view = LayoutInflater.from(activity)
                 .inflate(R.layout.rainbow_toast_layout, null);
-
+        Timer timer = new Timer();
         MaterialCardView materialCardView = view.findViewById(R.id.rainbowCard);
+        MaterialCardView materialCardView2 = view.findViewById(R.id.rainbowCard2);
+        MaterialCardView materialCardView3 = view.findViewById(R.id.rainbowCard3);
+        MaterialCardView materialCardView4 = view.findViewById(R.id.rainbowCard4);
+        MaterialCardView materialCardView5 = view.findViewById(R.id.rainbowCard5);
         TextView title = view.findViewById(R.id.rainbowTitle);
         TextView message = view.findViewById(R.id.rainbowMessage);
         Toast toast = new Toast(activity);
 
+        callColorChanger(activity, timer, mode, materialCardView, materialCardView2, materialCardView3, materialCardView4, materialCardView5);
         title.setText(titleData);
         message.setText(messageData);
         toast.setDuration(duration);
@@ -35,5 +47,51 @@ public class RainbowToast {
         toast.setView(view);
         toast.show();
     }
+    private static void setColorToCardStrokeLite(Activity activity, String mode, MaterialCardView... materialCardView){
 
+        if(mode.equals(DARK)){
+
+        }else{
+            materialCardView[0].setStrokeColor(activity.getResources().getColor(R.color.rainbow_card_lite_stroke_lite));
+            materialCardView[1].setStrokeColor(activity.getResources().getColor(R.color.rainbow_card2_lite_stroke_lite));
+            materialCardView[2].setStrokeColor(activity.getResources().getColor(R.color.rainbow_card3_lite_stroke_lite));
+            materialCardView[3].setStrokeColor(activity.getResources().getColor(R.color.rainbow_card4_lite_stroke_lite));
+            materialCardView[4].setStrokeColor(activity.getResources().getColor(R.color.rainbow_card5_lite_stroke_lite));
+        }
+    }
+    private static void setColorToCardStrokeDark(Activity activity, String mode, MaterialCardView... materialCardView){
+
+        if(mode.equals(DARK)){
+
+        }else{
+            materialCardView[0].setStrokeColor(activity.getResources().getColor(R.color.rainbow_card_lite_stroke_dark));
+            materialCardView[1].setStrokeColor(activity.getResources().getColor(R.color.rainbow_card2_lite_stroke_dark));
+            materialCardView[2].setStrokeColor(activity.getResources().getColor(R.color.rainbow_card3_lite_stroke_dark));
+            materialCardView[3].setStrokeColor(activity.getResources().getColor(R.color.rainbow_card4_lite_stroke_dark));
+            materialCardView[4].setStrokeColor(activity.getResources().getColor(R.color.rainbow_card5_lite_stroke_dark));
+        }
+    }
+
+    private static void callColorChanger(Activity activity,  Timer timer, String mode, MaterialCardView... materialCardView){
+
+        timer.scheduleAtFixedRate(new TimerTask(){
+            @Override
+            public void run(){
+                if(colorChooser == 0){
+                    colorChooser = 1;
+                    setColorToCardStrokeLite(activity, mode, materialCardView);
+                }
+                else {
+                    colorChooser = 0;
+                    setColorToCardStrokeDark(activity, mode, materialCardView);
+                }
+                cancelTimer += RUN_INTERVAL;
+                if(cancelTimer >= STOP_AFTER){
+                    timer.cancel();
+                    colorChooser = 0;
+                    cancelTimer = DELAY_TIMER;
+                }
+            }
+        },DELAY_TIMER,RUN_INTERVAL);
+    }
 }
