3fbb8756900a3aad28ea7cabd7712817501325b4
Imran <smdimran838@gmail.com>
2022-06-08 00:00:55 +0530

    Added complete lite and dark backgrounds

--- app/src/main/java/com/rainbowtoast/HalloweenToast.java
+++ app/src/main/java/com/rainbowtoast/HalloweenToast.java
@@ -8,17 +8,8 @@ import android.widget.Toast;
 
 import com.google.android.material.card.MaterialCardView;
 
-import java.util.Timer;
-
 public class HalloweenToast {
 
-    private static int colorChooser = 0;
-    private static final int DELAY_TIMER = 0;
-    private static int cancelTimer = DELAY_TIMER;
-    private static final int RUN_INTERVAL = 1000;
-    private static int stopAfter = 0;
-    private static final int STOP_AFTER2K = 2000;
-    private static final int STOP_AFTER4K = 4000;
     public static final String SUCCESS = "SUCCESS";
     public static final String ERROR = "ERROR";
     public static final String WARNING = "WARNING";
@@ -33,72 +24,92 @@ public class HalloweenToast {
 
         View view = LayoutInflater.from(activity)
                 .inflate(R.layout.halloween_toast_layout, null);
-        Timer timer = new Timer();
+
         MaterialCardView materialCardView = view.findViewById(R.id.rainbowCard);
-        materialCardView.setBackgroundResource(R.drawable.ic_bg);
+
         TextView title = view.findViewById(R.id.rainbowTitle);
         TextView message = view.findViewById(R.id.rainbowMessage);
-        Toast toast = new Toast(activity);
 
-        if(duration == LENGTH_LONG){
-            stopAfter = STOP_AFTER4K;
-        }else{
-            stopAfter = STOP_AFTER2K;
-        }
-
-//        setColorByType(view, activity, type, mode, materialCardView, title, message);
         title.setText(titleData);
         message.setText(messageData);
+
+        setBackgroundByType(type, mode, materialCardView);
+        setColorByType(view, activity, type, mode, materialCardView, title, message);
+
+        Toast toast = new Toast(activity);
         toast.setDuration(duration);
         toast.setView(view);
         toast.show();
     }
 
-    private static void setColorToCard(MaterialCardView materialCardView, Activity activity, int colorBackground, int colorStroke, TextView title, int titleColor){
+    private static void setBackgroundByType(String type, String mode, MaterialCardView materialCardView) {
+        if(mode.equals(DARK)){
+            switch (type) {
+                case SUCCESS:
+                    materialCardView.setBackgroundResource(R.drawable.ht_success_bg_dark);
+                    break;
+                case ERROR:
+                    materialCardView.setBackgroundResource(R.drawable.ht_error_bg_dark);
+                    break;
+                case WARNING:
+                    materialCardView.setBackgroundResource(R.drawable.ht_warning_bg_dark);
+                    break;
+                case INFO:
+                    materialCardView.setBackgroundResource(R.drawable.ht_info_bg_dark);
+                    break;
+                default:
+                    materialCardView.setBackgroundResource(R.drawable.ht_custom_bg_dark);
+                    break;
+            }
+        }else{
+            switch (type) {
+                case SUCCESS:
+                    materialCardView.setBackgroundResource(R.drawable.ht_success_bg_lite);
+                    break;
+                case ERROR:
+                    materialCardView.setBackgroundResource(R.drawable.ht_error_bg_lite);
+                    break;
+                case WARNING:
+                    materialCardView.setBackgroundResource(R.drawable.ht_warning_bg_lite);
+                    break;
+                case INFO:
+                    materialCardView.setBackgroundResource(R.drawable.ht_info_bg_lite);
+                    break;
+                default:
+                    materialCardView.setBackgroundResource(R.drawable.ht_custom_bg_lite);
+                    break;
+            }
+        }
+    }
+
+    private static void setColorToCard(Activity activity, TextView title, int titleColor){
         title.setTextColor(activity.getResources().getColor(titleColor));
-        materialCardView.setCardBackgroundColor(activity.getResources().getColor(colorBackground));
-        materialCardView.setStrokeColor(activity.getResources().getColor(colorStroke));
     }
 
     private static void setColorByType(View view, Activity activity, String type, String mode, MaterialCardView materialCardView, TextView title, TextView message) {
 
 
         if(mode.equals(DARK)){
-            message.setTextColor(activity.getResources().getColor(R.color.dark_title_para_text_color));
-            switch (type) {
-                case SUCCESS:
-                    setColorToCard(materialCardView, activity, R.color.card_backgroud_success_dark, R.color.card_stroke_success_dark, title, R.color.dark_title_para_text_color);
-                    break;
-                case ERROR:
-                    setColorToCard(materialCardView, activity, R.color.card_backgroud_error_dark, R.color.card_stroke_error_dark, title, R.color.dark_title_para_text_color);
-                    break;
-                case WARNING:
-                    setColorToCard(materialCardView, activity, R.color.card_backgroud_warning_dark, R.color.card_stroke_warning_dark, title, R.color.dark_title_para_text_color);
-                    break;
-                case INFO:
-                    setColorToCard(materialCardView, activity, R.color.card_backgroud_info_dark, R.color.card_stroke_info_dark, title, R.color.dark_title_para_text_color);
-                    break;
-                default:
-                    setColorToCard(materialCardView, activity, R.color.card_backgroud_custom_dark, R.color.card_stroke_custom_dark, title, R.color.dark_title_para_text_color);
-                    break;
-            }
+            title.setTextColor(activity.getResources().getColor(R.color.ht_title_para_text_color_dark));
+            message.setTextColor(activity.getResources().getColor(R.color.ht_title_para_text_color_dark));
+
         }else{
-            message.setTextColor(activity.getResources().getColor(R.color.para_text_color_lite));
+            message.setTextColor(activity.getResources().getColor(R.color.ht_para_text_color_lite));
             switch (type) {
                 case SUCCESS:
-                    setColorToCard(materialCardView, activity, R.color.card_backgroud_success_lite, R.color.card_stroke_success_lite, title, R.color.success_title_text_color_lite);
+                    setColorToCard(activity, title, R.color.ht_success_title_text_color_lite);
                     break;
                 case ERROR:
-                    setColorToCard(materialCardView, activity, R.color.card_backgroud_error_lite, R.color.card_stroke_error_lite, title, R.color.error_title_text_color_lite);
+                    setColorToCard(activity, title, R.color.ht_error_title_text_color_lite);
                     break;
                 case WARNING:
-                    setColorToCard(materialCardView, activity, R.color.card_backgroud_warning_lite, R.color.card_stroke_warning_lite, title, R.color.warning_title_text_color_lite);
+                    setColorToCard(activity, title, R.color.ht_warning_title_text_color_lite);
                     break;
                 case INFO:
-                    setColorToCard(materialCardView, activity, R.color.card_backgroud_info_lite, R.color.card_stroke_info_lite, title, R.color.info_title_text_color_lite);
+                    setColorToCard(activity, title, R.color.ht_info_title_text_color_lite);
                     break;
                 default:
-                    setColorToCard(materialCardView, activity, R.color.card_backgroud_custom_lite, R.color.card_stroke_custom_lite, title, R.color.custom_title_text_color_lite);
+                    setColorToCard(activity, title, R.color.ht_custom_title_text_color_lite);
                     break;
             }
         }
--- app/src/main/java/com/rainbowtoast/MainActivity.java
+++ app/src/main/java/com/rainbowtoast/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         HalloweenToast.showToast(this, "Success", "Connection established successfully!",
-                HalloweenToast.LENGTH_SHORT, HalloweenToast.SUCCESS, HalloweenToast.LITE );
+                HalloweenToast.LENGTH_SHORT, HalloweenToast.CUSTOM, HalloweenToast.LITE );
     }
 }
